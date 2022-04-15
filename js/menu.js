a11a82a9264b0761cdb6a77aa6989f8092a32bc3
cha-sung-jun <103724207+cha-sung-jun@users.noreply.github.com>
2022-04-15 16:24:01 +0900

    Delete menu.js

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $(".nav li").mouseenter(function(){
-      $(this).children(".sub").stop().slideDown();
-    });
-
-    $(".nav li").mouseleave(function(){
-      $(this).children(".sub").stop().slideUp();
-    });
-});
